3c27d0c76a99cf868111ed58c17639aa99f677fc
Đỗ Đức Khôi <khoihai123456@gmail.com>
2025-01-23 23:54:06 +0700

    Update diagonalproblem1.py

--- diagonalproblem1.py
+++ diagonalproblem1.py
@@ -17,10 +17,10 @@ def diagonalDifference(arr):
     # Write your code here
     sum1 = 0
     sum2 = 0
-    for i in range(len(arr)):
+    for i in range(n):
        sum1 += arr[i][i]
-       sum2 += arr[i][len(arr)-1 + i]   
-       return abs(sum1+sum2)
+       sum2 += arr[i][n-1 + i]   
+       return abs(sum1-sum2)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
